55f6e3f0b4431ca20aa3f2fd1c90aab03854e64c
Athul <unqualv@gmail.com>
2021-05-20 14:32:08 +0530

    Finished With Grid

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let i;
-for (i = 0; i < 25; i++) {
+
+for (let i = 0; i < 100; i++) {
     const container = document.querySelector('#container');
     const content = document.createElement('div');
     content.classList.add('grid-item');
