817d04ee03141a6f615c6b35c822c88b29af8a59
unequalv <unequalv@gmail.com>
2021-05-20 22:01:55 +0530

    Finished the Main Box

--- script.js
+++ script.js
@@ -2,6 +2,14 @@
 for (let i = 0; i < 100; i++) {
     const container = document.querySelector('#container');
     const content = document.createElement('div');
-    content.classList.add('grid-item');
+    content.classList.add('item');
     container.appendChild(content);
+}
+
+const item = document.getElementsByClassName('item');
+for (let i = 0; i < 100; i++) {
+    item[i].addEventListener("mouseover", mouseover);
+    function mouseover() {
+        item[i].style.backgroundColor = "black";
+    }
 }
